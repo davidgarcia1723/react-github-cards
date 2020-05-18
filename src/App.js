5ae437d4ee19edbac9fd727f027fdcaa431e0c86
David Garcia <davidgarcia1723@gmail.com>
2020-05-18 11:43:28 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Axios from 'axios';
-
-const testData = [
-  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-  {name: "Latori Miller", avatar_url: "https://avatars0.githubusercontent.com/u/16719162?v=4", company: "Road to Hire"},
-  {name: "Dylan Trimble", avatar_url: "https://avatars1.githubusercontent.com/u/42077977?v=4", company: "Road to Hire"},
-];
-
+const axios = require('axios');
 class Form extends React.Component {
   userNameInput = React.createRef();
   handleSubmit = async (event) => {
@@ -36,43 +29,13 @@ class Form extends React.Component {
     );
   }
 }
-
 const CardList = (props) => (
   <div>
-   {props.profiles.map(profile => <Card key={profile.id} {...profile} />)}
+    {props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
   </div>
 )
-
-
-class App extends React.Component {
-  constructor(props){
-    super(props);
-      this.state = {
-        profiles : testData
-      }
-  }
-
-addNewProfileData = (profileData) => { 
-  this.setState(prevState => ({ profiles : [...prevState.profiles, profileData],
-  }))
-  console.log('App', profileData)
-}
-
-  render() {
-    return (
-      <div>
-        <div className="header">{this.props.title}</div>
-        <Form onSubmit={this.addNewProfileData} />
-        <CardList profiles={this.state.profiles} />
-      </div>
-    )
-
-  }
-}
-
-
 class Card extends React.Component {
-  render() {
+  render(){
     const profile = this.props;
     return (
       <div className="github-profile">
@@ -82,9 +45,29 @@ class Card extends React.Component {
           <div className="company">{profile.company}</div>
         </div>
       </div>
+    );
+  }
+}
+class App extends React.Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        profiles: []
+      };
+  }  
+  addNewProfileData = (profileData) => {
+    this.setState(prevState => ({profiles: [...prevState.profiles, profileData],
+    }));
+    console.log('App', profileData);
+  }
+  render(){
+    return (
+    <div>
+      <div className="header">{this.props.title}</div>
+      <Form onSubmit={this.addNewProfileData} />
+      <CardList profiles={this.state.profiles} />
+    </div>
     )
   }
 }
-
-
 export default App;
